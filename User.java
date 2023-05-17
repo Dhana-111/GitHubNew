be216b36b94ab00dfe27995127964d5bcfd62f7b
Dhana-111 <125950690+Dhana-111@users.noreply.github.com>
2023-05-17 16:52:39 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,18 @@
+package com.ssd;
+
+public class User{
+	  public static void main(String args[]) {
+			Project obj=new Project();
+		    obj.exit();
+			obj.payTuition();
+			System.out.println(obj.toString());
+			Project obj1=new Project();
+			obj1.exit();
+			obj1.payTuition();
+			System.out.println(obj1.toString());
+		}
+	}
+
+
+
+
